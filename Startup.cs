898c4ad61b67bbba6925cc8b1f80fac2dab98e23
ehsan karimi <karimiehsan901@gmail.com>
2019-01-26 12:53:35 +0330

    gitignore changed

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace stackoverflow
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSession();
         }
-
+    
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
