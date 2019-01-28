e65a836c196ab46115afa18f53e4e9ed01c72d23
mobarake sadat <mobarake.mirkamali@gmail.com>
2019-01-28 22:01:02 +0330

    Models/dao/LikeAnswerDAO.cs

--- Models/dao/LikeAnswerDAO.cs
+++ Models/dao/LikeAnswerDAO.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using stackoverflow.Models.db;
+
 namespace stackoverflow.Models.dao
 {
     public class LikeAnswerDAO : DAO
@@ -12,6 +17,45 @@ namespace stackoverflow.Models.dao
             }
 
             return instance;
+    
+        }
+
+        private LikeAnswerDAO()
+        {
+
+        }
+
+        public int PlusLikesAnswer(int answerId,int userId)
+        {
+            int count = 0;
+            var cmd = new MySqlCommand("select * from main_likeanswer where answer_id=" + answerId + "and user_id=" + userId , DBConnection.Instance().MySqlConnection);
+            
+            var rd = cmd.ExecuteReader();
+
+            if (rd.Read())
+            {
+                var cmd2 = new MySqlCommand("update main_likeanswer where answer_id=" + answerId + "and user_id=" + userId + "set is_like=true", DBConnection.Instance().MySqlConnection);
+                cmd2.ExecuteNonQuery();
+            }
+            else
+            {
+                var cmd2 = new MySqlCommand("insert into main_likeanswer (answer_id, user_id, is_like) values (\'" + answerId + "\', \'" + userId
+                                     + "\', \'true)", DBConnection.Instance().MySqlConnection);
+                cmd2.ExecuteNonQuery();
+
+            }
+            rd.Close();
+
+            var cmd3 = new MySqlCommand("select * from main_likeanswer where answer_id=" + answerId + "and is_like=true", DBConnection.Instance().MySqlConnection);
+            var rd2 = cmd3.ExecuteReader();
+
+            while (rd2.Read())
+            {
+                count++;
+            }
+            rd2.Close();
+
+            return count;
         }
         
     }
