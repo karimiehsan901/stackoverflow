09d0d1fff0222dedb1d6a347d56cb64f5045af4a
Masoumeh Kouhestani <m.kouhestani@mail.sbu.ac.ir>
2019-01-28 22:15:23 +0330

    testMasoumeh

--- Models/dao/TagQuestionDAO.cs
+++ Models/dao/TagQuestionDAO.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using stackoverflow.Models.db;
+
 namespace stackoverflow.Models.dao
 {
     public class TagQuestionDAO
@@ -16,6 +21,22 @@ namespace stackoverflow.Models.dao
 
         private TagQuestionDAO()
         {
+        }
+
+        public List<Tag> GetTagsOfQuestion(int questionId)
+        {
+            var ans = new List<Tag>();
+            var idFinder = new MySqlCommand("select * from main_question where user_id=" + questionId + " order by id desc", DBConnection.Instance().MySqlConnection);
+            var rd = idFinder.ExecuteReader();
+            while (rd.Read())
+            {
+                var title = (string)rd["title"];
+                var id = (int)rd["id"];
+                var tag = new Tag(id, title);
+                ans.Add(tag);
+            }
+            rd.Close();
+            return ans;
         }
     }
 }
