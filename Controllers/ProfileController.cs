4724efd52e8f70370513df3bd1fa9c4530b16815
Tara <targhesh@gmail.com>
2019-01-30 02:20:54 +0330

    profile controller:data of user

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,11 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using stackoverflow.Models;
+using stackoverflow.Models.dao;
+using stackoverflow.Models.db;
 
 namespace stackoverflow.Controllers
 {
     public class ProfileController : Controller
     {
+        private UserDAO _userDAO = UserDAO.Instance();
+        private SessionDAO _sessionDAO = SessionDAO.Instance();
+        private QuestionDAO _questionDAO = QuestionDAO.Instance();
+        private AnswerDAO _answerDAO = AnswerDAO.Instance();
+        private TagQuestionDAO _tagQuestionDAO = TagQuestionDAO.Instance();
+        private TagDAO _tagDAO = TagDAO.Instance();
+        private LikeQuestionDAO likeQuestionDAO = LikeQuestionDAO.Instance();
+
         public IActionResult Index()
         {
+            var sessionId = HttpContext.Session.Id;
+            var username = _sessionDAO.GetUsername(sessionId);
+            var user = _userDAO.GetUserByUsername(username);
+            var questions = _questionDAO.GetQuestionsOfUser(user.Id);
+            var answers = _answerDAO.GetAnswersOfUser(user.Id);
+
+            var dic = new Dictionary<string, object>
+            {
+                ["id"] = user.Id,
+                ["name"] = user.Name,
+                ["password"] = user.Password,
+                ["username"] = user.Username,
+                ["email"] = user.Email,
+                ["questions"] = questions,
+                ["answers"] = answers
+            };
+            ViewData["dataOfUser"] = dic;
             return View();
         }
         
